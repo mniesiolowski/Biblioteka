05c9db14585805ae53c186f80041d21eaba57a71
Marcin <m.niesiolowski@o2.pl>
2018-06-22 21:13:41 +0200

    NewVersion

--- src/main/java/pl/coderslab/repository/AdressRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.coderslab.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.coderslab.entity.Adress;
-
-public interface AdressRepository extends JpaRepository<Adress, Long> {
-}
--- src/main/java/pl/coderslab/repository/DrinkRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.coderslab.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.coderslab.entity.Drink;
-
-@Repository
-public interface DrinkRepository extends JpaRepository<Drink, Long> {
-}
